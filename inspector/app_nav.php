d0fc5995b515d03fad20b76887693552d0054bab
rallagas <80924362+rallagas@users.noreply.github.com>
2026-01-08 09:58:07 +0800

    Update app_nav.php

--- inspector/app_nav.php
+++ inspector/app_nav.php
@@ -35,7 +35,7 @@
             <li class="nav-item">
                     <a id="inspectorLogout" class="nav-link" href="#" data-bs-toggle="modal" data-bs-target="#logoutConfirmModal">
                     <?= getIcon("logout") ?>   
-                    <small class="d-inline d-lg-none d-md-none">LOGOUT</small>
+                    <small class="ms-2">Logout</small>
                     </a>
                 </li>
         </ul>
